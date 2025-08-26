1a83fb070f5c68405e1a244ab87716ba6749c8e4
Navarro <navarrorangel1@hotmail.com>
2025-08-26 10:46:03 -0300

    :sparkles: feat: creation of ProductController and ProductService. Not connected yet with outsider API

---
+++ src/main/java/dev/java/ecommerce/basketservice/Service/ProductService.java
@@ -0,0 +1,14 @@
+package dev.java.ecommerce.basketservice.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public void getAllProducts() {
+
+    }
+
+    public void getProductById() {
+    }
+}
