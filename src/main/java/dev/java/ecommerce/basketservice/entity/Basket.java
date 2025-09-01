1cc8c53951dc2aaaf5f481a42318720ba84b53b3
Navarro <navarrorangel1@hotmail.com>
2025-09-01 09:44:26 -0300

    :bug: fix: Wrong doccument collection name, before i was usin collation insted collection

--- src/main/java/dev/java/ecommerce/basketservice/entity/Basket.java
+++ src/main/java/dev/java/ecommerce/basketservice/entity/Basket.java
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Document(collation = "basket")
+@Document(collection = "basket")
 public class Basket {
     @Id
     private String basketId;
