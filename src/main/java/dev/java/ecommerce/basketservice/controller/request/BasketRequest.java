5d9aabfd9510f8691e3d7ab899016e4bb52079a4
Navarro <navarrorangel1@hotmail.com>
2025-09-01 09:53:19 -0300

    :sparkles: feat: added feats to use BasketRequest and Repository

--- src/main/java/dev/java/ecommerce/basketservice/controller/request/BasketRequest.java
+++ src/main/java/dev/java/ecommerce/basketservice/controller/request/BasketRequest.java
@@ -4,5 +4,5 @@ package dev.java.ecommerce.basketservice.controller.request;
 
 import java.util.List;
 
-public record BasketRequest(Long clientId, List<ProductRequest> products {
+public record BasketRequest(Long clientId, List<ProductRequest> products ){
 }
